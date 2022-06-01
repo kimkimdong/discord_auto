833105c1104303e3efdf096a61223e60c86edb5f
DESKTOP-BUTN0KF\kim_dong_il <kimkim543b@naver.com>
2022-06-01 12:10:59 +0900

    no message

---
+++ discord_auto_ver0.4.py
@@ -0,0 +1,241 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_autoinstaller  #자동 인스톨
+import time
+import subprocess  #윈도우서 실행
+import datetime
+from pytz import timezone
+from bs4 import BeautifulSoup
+import requests
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+# 945956410578067486
+
+time_now =  datetime.datetime.now(timezone('Asia/Seoul'))
+
+
+res=requests.get("https://blog.naver.com/hoy2942/222623679530")
+soup = BeautifulSoup(res.text,'html.parser')
+if 'pinestone_golf' in soup.get_text():
+    form_class = uic.loadUiType("./sys/discord_auto.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        
+        with open('./sys/id_lists.txt','r',encoding='utf-8') as f:
+            id_lists = f.read().splitlines() # 개행연산자 빼고 리스트가 됨
+        for i in id_lists:
+            self.comboBox.addItem(i)
+            
+        self.pushButton.clicked.connect(self.create_session)
+        self.pushButton_2.clicked.connect(self.start_discord_auto)
+        # self.pushButton_3.clicked.connect(self.quit_chrome)
+        
+    def start_discord_auto(self): # 작업시작
+        with open('./sys/id_lists.txt','r',encoding='utf-8') as f:  #id 가지고 오기/
+            ids = f.read().splitlines() 
+            
+        if len(self.lineEdit.text()) + len(self.lineEdit_2.text()) + len(self.lineEdit_3.text()) == 0 and len(self.lineEdit_4.text()) + len(self.lineEdit_5.text()) != 0:   #  이벤트 가입만 하는경우
+            for i in ids[:50]:
+                self.open_chrome(i)
+                if self.driver.current_url == 'https://discord.com/channels/@me':
+                    try:
+                        self.event_join()
+                        self.driver.close()
+                    except:
+                        self.driver.close()
+                else :
+                    self.driver.close()
+                time.sleep(self.spinBox.value())
+                    
+        elif len(self.lineEdit_4.text()) + len(self.lineEdit_5.text()) == 0 and len(self.lineEdit.text()) + len(self.lineEdit_2.text()) + len(self.lineEdit_3.text()) != 0:   #  서버가입만 하는경우
+            for i in ids[:50]:
+                self.open_chrome(i)
+                if self.driver.current_url == 'https://discord.com/channels/@me':
+                    try:
+                        self.server_join()
+                        self.driver.close()
+                    except:
+                        self.driver.close()
+                else :
+                    self.driver.close()
+                time.sleep(self.spinBox.value())
+        elif len(self.lineEdit_4.text()) + len(self.lineEdit_5.text()) != 0 and len(self.lineEdit.text()) + len(self.lineEdit_2.text()) + len(self.lineEdit_3.text()) != 0:  # 모두 입력되어있을때
+            for i in ids[:50]:
+                self.open_chrome(i)
+                if self.driver.current_url == 'https://discord.com/channels/@me':
+                    try:
+                        self.server_join_2()
+                    except:
+                        pass
+                    try:
+                        self.event_join()
+                        self.driver.close()
+                    except:
+                        self.driver.close()
+                else :
+                    self.driver.close()
+                time.sleep(self.spinBox.value())
+
+    def server_join(self):
+        # if len(self.lineEdit_4.text()) != 0:
+        #     if self.lineEdit_4.text().split('/')[5][:18] not in self.driver.page_source:
+        if self.comboBox_2.currentIndex() == 0:
+            time.sleep(3)
+            self.driver.get(self.lineEdit.text())  #라인에디터1 삽입 초대장
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/section/div/button/div').click()
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div[1]/div/div[1]/button').click() # 완료버튼
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[4]/div[2]/div/div/div[2]/div[1]/div[1]/div/div/div/div[3]/div/label/input').click() #규칙을 읽었으면~~
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[4]/div[2]/div/div/div[2]/div[2]/button').click() # 전송버튼
+            self.driver.get(self.lineEdit_2.text())  #라인에디터2 서버가입주소
+            time.sleep(3)
+            self.driver.find_element_by_xpath(self.lineEdit_3.text()).click()  #라인에디터3 서버가입 클릭
+            time.sleep(3)
+        elif self.comboBox_2.currentIndex() == 1:
+            time.sleep(3)
+            self.driver.get(self.lineEdit.text())  #라인에디터1 삽입 초대장
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/section/div/button/div').click()
+            time.sleep(3)
+            self.driver.get(self.lineEdit_2.text())  #라인에디터2 서버가입주소
+            time.sleep(3)
+            self.driver.find_element_by_xpath(self.lineEdit_3.text()).click()  #라인에디터3 서버가입 클릭
+            time.sleep(3)
+    def server_join_2(self):
+        # if len(self.lineEdit_4.text()) != 0:
+        #     if self.lineEdit_4.text().split('/')[5][:18] not in self.driver.page_source:
+        if self.comboBox_2.currentIndex() == 0:
+            time.sleep(3)
+            self.driver.get(self.lineEdit.text())  #라인에디터1 삽입 초대장
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/section/div/button/div').click()
+            time.sleep(3)
+            if len(self.lineEdit_4.text()) != 0:
+                if self.lineEdit_4.text().split('/')[5][:18] not in self.driver.page_source:
+                    self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div[1]/div/div[1]/button').click() # 완료버튼
+                    time.sleep(3)
+                    self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[4]/div[2]/div/div/div[2]/div[1]/div[1]/div/div/div/div[3]/div/label/input').click() #규칙을 읽었으면~~
+                    time.sleep(3)
+                    self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[4]/div[2]/div/div/div[2]/div[2]/button').click() # 전송버튼
+                    self.driver.get(self.lineEdit_2.text())  #라인에디터2 서버가입주소
+                    time.sleep(3)
+                    self.driver.find_element_by_xpath(self.lineEdit_3.text()).click()  #라인에디터3 서버가입 클릭
+                    time.sleep(3)
+        elif self.comboBox_2.currentIndex() == 1:
+            time.sleep(3)
+            self.driver.get(self.lineEdit.text())  #라인에디터1 삽입 초대장
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/section/div/button/div').click()
+            time.sleep(3)
+            if len(self.lineEdit_4.text()) != 0:
+                if self.lineEdit_4.text().split('/')[5][:18] not in self.driver.page_source:
+                    self.driver.get(self.lineEdit_2.text())  #라인에디터2 서버가입주소
+                    time.sleep(3)
+                    self.driver.find_element_by_xpath(self.lineEdit_3.text()).click()  #라인에디터3 서버가입 클릭
+                    time.sleep(3)
+    def server_join(self):
+        # if len(self.lineEdit_4.text()) != 0:
+        #     if self.lineEdit_4.text().split('/')[5][:18] not in self.driver.page_source:
+        if self.comboBox_2.currentIndex() == 0:
+            time.sleep(3)
+            self.driver.get(self.lineEdit.text())  #라인에디터1 삽입 초대장
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/section/div/button/div').click()
+            time.sleep(3)
+            if len(self.lineEdit_4.text()) != 0:
+                if self.lineEdit_4.text().split('/')[5][:18] not in self.driver.page_source:
+                    self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div[1]/div/div[1]/button').click() # 완료버튼
+                    time.sleep(3)
+                    self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[4]/div[2]/div/div/div[2]/div[1]/div[1]/div/div/div/div[3]/div/label/input').click() #규칙을 읽었으면~~
+                    time.sleep(3)
+                    self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[4]/div[2]/div/div/div[2]/div[2]/button').click() # 전송버튼
+                    self.driver.get(self.lineEdit_2.text())  #라인에디터2 서버가입주소
+                    time.sleep(3)
+                    self.driver.find_element_by_xpath(self.lineEdit_3.text()).click()  #라인에디터3 서버가입 클릭
+                    time.sleep(3)
+        elif self.comboBox_2.currentIndex() == 1:
+            time.sleep(3)
+            self.driver.get(self.lineEdit.text())  #라인에디터1 삽입 초대장
+            time.sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/section/div/button/div').click()
+            time.sleep(3)
+            if len(self.lineEdit_4.text()) != 0:
+                if self.lineEdit_4.text().split('/')[5][:18] not in self.driver.page_source:
+                    self.driver.get(self.lineEdit_2.text())  #라인에디터2 서버가입주소
+                    time.sleep(3)
+                    self.driver.find_element_by_xpath(self.lineEdit_3.text()).click()  #라인에디터3 서버가입 클릭
+                    time.sleep(3)
+                #     else:
+        #         pass
+        # else:
+        #     pass
+
+    def event_join(self):
+        time.sleep(3)
+        self.driver.get(self.lineEdit_4.text())  #라인에디터4 이벤트 주소
+        time.sleep(3)
+        ActionChains(self.driver).send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(1)
+        ActionChains(self.driver).send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(1)
+        ActionChains(self.driver).send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(1)
+        ActionChains(self.driver).send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(1)
+        ActionChains(self.driver).send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(1)
+        ActionChains(self.driver).send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(3)
+        self.driver.find_element_by_xpath(self.lineEdit_5.text()).click()  #라인에디터5 이벤트참여버튼 클릭
+        time.sleep(3)
+
+    def create_session(self):  # 세션 생성 
+        discord_id=self.comboBox.currentText()
+        self.open_chrome(discord_id)
+
+    def open_chrome(self, discord_id):  #크롬 실행
+        try:  #  구글로글인 때문에 디버그 모드로 접근하는거 실행
+            subprocess.Popen(f'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\discord_data\{discord_id}"')
+        except:
+            subprocess.Popen(f'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\discord_data\{discord_id}"')
+        option = Options()
+        option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+        option.add_argument("disable-gpu")
+        # option.add_argument(f'--user-data-dir=C:\\discord_data\\{discord_id}')   #아이디별로 데이터 만들기
+        chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]                      
+        try:
+            self.driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
+        except:
+            chromedriver_autoinstaller.install(True)
+            self.driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
+        self.driver.set_window_size(1400,900)
+        self.driver.implicitly_wait(15)
+        self.driver.get('https://discord.com/channels/@me')
+        if time_now.year == 2022 and time_now.month == 5 :
+            pass
+        elif time_now.year == 2022 and time_now.month == 6 :
+            pass
+        elif time_now.year == 2022 and time_now.month == 7 :
+            pass
+        elif time_now.year == 2022 and time_now.month == 8 :
+            pass
+        elif time_now.year == 2022 and time_now.month == 9 :
+            pass
+        else:
+            self.driver.close()
+        
+if __name__ == "__main__" :
+    app = QApplication(sys.argv) 
+    myWindow = WindowClass() 
+    myWindow.show()
+    app.exec_()
--- test01.py
+++ test01.py
@@ -2,23 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller  #자동 인스톨
 import subprocess  #윈도우서 실행
-
-try:  #  구글로글인 때문에 디버그 모드로 접근하는거 실행
-    subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\discord_data\modoc_blinov_32@autorambler.ru"') # 디버거 크롬 구동
-except:
-
-
-    subprocess.Popen(r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\discord_data\modoc_blinov_32@autorambler.ru"')
-
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+# try:  #  구글로글인 때문에 디버그 모드로 접근하는거 실행
+#     subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\discord_data\ruslankolodkin.1987@rambler.ru"')
+# except:
+#     subprocess.Popen(r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\discord_data\ruslankolodkin.1987@rambler.ru"')
 option = Options()
 option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-
-chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]                      #디버그 모드 빼고 자동로그인만 도 가능
+option.add_argument("disable-gpu")
+# option.add_argument(f'--user-data-dir=C:\\discord_data\\{discord_id}')   #아이디별로 데이터 만들기
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]                      
 try:
     driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
 except:
     chromedriver_autoinstaller.install(True)
     driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
 driver.set_window_size(1400,900)
-driver.implicitly_wait(10)
-driver.get('https://www.naver.com/')
+driver.implicitly_wait(15)
+driver.get('https://discord.com/channels/912314917393154128/974523774412161034')
+time.sleep(1)
+ActionChains(driver).send_keys(Keys.PAGE_DOWN).perform()
+time.sleep(1)
+ActionChains(driver).send_keys(Keys.PAGE_DOWN).perform()
+time.sleep(1)
+ActionChains(driver).send_keys(Keys.PAGE_DOWN).perform()
+time.sleep(1)
+ActionChains(driver).send_keys(Keys.PAGE_DOWN).perform()
+time.sleep(1)
+ActionChains(driver).send_keys(Keys.PAGE_DOWN).perform()
+time.sleep(1)
+ActionChains(driver).send_keys(Keys.PAGE_DOWN).perform()
+print(driver.page_source)
